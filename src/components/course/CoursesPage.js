1785242e79e81fbe5bd3de786ea799d1f9852ddd
maksym kolodiy <maksym.kolodiy.intern@3ds.com>
2018-10-10 16:48:51 +0200

    added more functionality to add course

--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -1,11 +1,34 @@
 import React, { Component, PropTypes } from 'react';
 
 export default class CoursesPage extends Component {
+  constructor(props,context){
+    super(props,context);
+
+    this.state = {
+      course: { title: null }
+    };
+  }
   render() {
     return (
       <div>
         <h1>Courses</h1>
+        <h2>Add Course</h2>
+        <input 
+          type="text"
+          onChange={this.onTitleChange}
+          value={this.state.course.title}
+        />
+        <input 
+          type="submit"
+          value="Save"
+          onClick={this.onClickSave}
+        />
       </div>
     );
+  }
+  onTitleChange(ev){
+    const course = this.state.course;
+    course.title = ev.target.value;
+    this.setState({course});
   }
 }
