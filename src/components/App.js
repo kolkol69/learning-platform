221d5e6beb5482c22adae20713346252cf4b3279
maksym kolodiy <maksym.kolodiy.intern@3ds.com>
2018-10-15 17:14:31 +0200

    now when ajax call is in progress the user is seing dots Loading as ajax is being pocessed

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,28 @@
 import React, { Component, PropTypes } from 'react';
 import Header from './common/Header';
+import {connect} from 'react-redux';
 
-export default class App extends Component {
+class App extends Component {
     render() {
         return (
             <div className="container-fluid">
-                <Header/>
+                <Header
+                    loading={this.props.loading}
+                />
                 {this.props.children}
             </div>
         );
     }
 }
 App.propTypes = {
-    children: PropTypes.object.isRequired
+    children: PropTypes.object.isRequired,
+    loading: PropTypes.bool.isRequired
 };
+
+function mapStateToProps(state, ownProps){
+    return {
+        loading: state.ajaxCallsInProgress > 0
+    };
+}
+
+export default connect(mapStateToProps)(App);
--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,8 +1,8 @@
-import React from 'react';
+import React, {PropTypes} from 'react';
 import { Link, IndexLink } from 'react-router';
 import LoadnigDots from './LoadingDots'
 
-const Header = () => {
+const Header = ({loading}) => {
     return (
         <nav>
             <IndexLink to="/" activeClassName="active">Home</IndexLink>
@@ -10,9 +10,13 @@ const Header = () => {
             <Link to="/courses" activeClassName="active">Courses</Link>
             {" | "}
             <Link to="/about" activeClassName="active">About</Link>
-            <LoadnigDots interval={100} dots={20}/>
+            {loading && <LoadnigDots interval={100} dots={20}/>}
         </nav>
     );
 };
+
+Header.propTypes = {
+    loading: PropTypes.bool.isRequired
+}
 
 export default Header;
