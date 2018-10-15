a76dcfb28ed680907d03b26a56b158548c9e0896
maksym kolodiy <maksym.kolodiy.intern@3ds.com>
2018-10-15 11:07:55 +0200

    minor changes

--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -1,13 +1,15 @@
 import React, { Component, PropTypes } from 'react';
+import { connect } from 'react-redux';
+import * as courseActions from '../../actions/courseActions'
 
-export default class CoursesPage extends Component {
+class CoursesPage extends Component {
   constructor(props, context) {
     super(props, context);
 
     this.state = {
       course: { title: null }
     };
-    
+
     this.onTitleChange = this.onTitleChange.bind(this);
     this.onClickSave = this.onClickSave.bind(this);
   }
@@ -16,18 +18,25 @@ export default class CoursesPage extends Component {
     course.title = ev.target.value;
     this.setState({ course });
   }
+
   onClickSave() {
-    alert(`Saving ${this.state.course.title}`);
+    this.props.createCourse(this.state.course);
   }
+
+  courseRow(course, index) {
+    return <div key={index}>{course.title}</div>;
+  }
+
   render() {
     return (
       <div>
         <h1>Courses</h1>
+        {this.props.courses.map(this.courseRow)}
         <h2>Add Course</h2>
         <input
           type="text"
-          onChange={this.onTitleChange}
           value={this.state.course.title}
+          onChange={this.onTitleChange}
         />
         <input
           type="submit"
@@ -38,3 +47,22 @@ export default class CoursesPage extends Component {
     );
   }
 }
+
+CoursesPage.propTypes = {
+  createCourse: PropTypes.func.isRequired,
+  courses: PropTypes.array.isRequired
+};
+
+function mapStateToProps(state, ownProps) {
+  return {
+    courses: state.courses
+  };
+}
+
+function mapDispatchToProps(dispatch){
+  return {
+    createCourse: course => dispatch(courseActions.createCourse(course))
+  };
+}
+
+export default connect(mapStateToProps,mapDispatchToProps)(CoursesPage); 
--- src/reducers/courseReducer.js
+++ src/reducers/courseReducer.js
@@ -1,10 +1,8 @@
 export default function courseReducers(state = [], action) {
-  switch (action.type) {
-    case 'CREATE_COURSE':
-      return [...state, Object.assign({}, action.course)];
-    default:
-      return state;
-    
-
-  }
+    switch (action.type) {
+        case 'CREATE_COURSE':
+            return [...state, Object.assign({}, action.course)];
+        default:
+            return state;
+    }
 }
