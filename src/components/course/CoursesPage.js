319eccaf3431d4b5a35724550c6a5b06d55c8b1f
maksym kolodiy <maksym.kolodiy.intern@3ds.com>
2018-10-15 11:21:23 +0200

    added bindActionCreator to CoursesPage, now types are const in separate file

--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -1,6 +1,7 @@
 import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
-import * as courseActions from '../../actions/courseActions'
+import { bindActionCreators } from 'redux';
+import * as courseActions from '../../actions/courseActions';
 
 class CoursesPage extends Component {
   constructor(props, context) {
@@ -20,7 +21,7 @@ class CoursesPage extends Component {
   }
 
   onClickSave() {
-    this.props.createCourse(this.state.course);
+    this.props.actions.createCourse(this.state.course);
   }
 
   courseRow(course, index) {
@@ -49,8 +50,8 @@ class CoursesPage extends Component {
 }
 
 CoursesPage.propTypes = {
-  createCourse: PropTypes.func.isRequired,
-  courses: PropTypes.array.isRequired
+  courses: PropTypes.array.isRequired,
+  actions: PropTypes.object.isRequired
 };
 
 function mapStateToProps(state, ownProps) {
@@ -59,10 +60,10 @@ function mapStateToProps(state, ownProps) {
   };
 }
 
-function mapDispatchToProps(dispatch){
+function mapDispatchToProps(dispatch) {
   return {
-    createCourse: course => dispatch(courseActions.createCourse(course))
+    actions: bindActionCreators(courseActions, dispatch)
   };
 }
 
-export default connect(mapStateToProps,mapDispatchToProps)(CoursesPage); 
+export default connect(mapStateToProps, mapDispatchToProps)(CoursesPage); 
