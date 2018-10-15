ec3f97c79448f862ae57599eebfa3b83d91ebd48
maksym kolodiy <maksym.kolodiy.intern@3ds.com>
2018-10-15 16:20:09 +0200

    when editing the course you have all needed info pasted for you, so you can edit it easily

--- src/components/course/ManageCoursePage.js
+++ src/components/course/ManageCoursePage.js
@@ -55,8 +55,18 @@ ManageCoursePage.contextTypes = {
     router: PropTypes.object
 };
 
-const mapStateToProps = (state) => {
+function getCourseById(courses, id){
+    const course = courses.filter(courses => courses.id == id);
+    return course ? course[0] : null;
+}
+
+const mapStateToProps = (state, ownProps) => {
+    const courseId = ownProps.params.id; //from the path `/course/:id`
     let course = { id: '', watchHref: '', title: '', authorId: '', length: '', category: '' };
+
+    if (courseId && state.courses.length > 0){
+        course = getCourseById(state.courses, courseId);
+    }
 
     const authorsFromattedForDropdown = state.authors.map(author => {
         return {
