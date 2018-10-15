6ae9f3e02ab99ed10478db70eb4da7f622d2e70b
maksym kolodiy <maksym.kolodiy.intern@3ds.com>
2018-10-15 17:05:49 +0200

    dispatch ajax call is added in author\course actions

--- src/actions/courseActions.js
+++ src/actions/courseActions.js
@@ -1,5 +1,6 @@
 import * as types from './actionTypes';
 import courseApi from '../api/mockCourseApi';
+import {beginAjaxCall} from './ajaxStatusActions'
 
 export function loadCoursesSuccess(courses) {
     return {
@@ -21,6 +22,7 @@ export const updateCourseSuccess = (course) => ({
 
 export function loadCourses() {
     return function (dispatch) {
+        dispatch(beginAjaxCall());
         return courseApi.getAllCourses().then(courses => {
             dispatch(loadCoursesSuccess(courses));
         }).catch(error => {
@@ -31,6 +33,7 @@ export function loadCourses() {
 
 export function saveCourse(course) {
     return function (dispatch) {
+        dispatch(beginAjaxCall());
         return courseApi.saveCourse(course).then(savedCourse => {
             course.id ? dispatch(updateCourseSuccess(savedCourse)) :
                 dispatch(createCourseSuccess(savedCourse))
