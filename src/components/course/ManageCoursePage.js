106312fab081e1127a866adb9ebc53f7a3e30431
maksym kolodiy <maksym.kolodiy.intern@3ds.com>
2018-10-15 17:22:33 +0200

    added response when clicking save button and local state to handle it

--- src/components/course/ManageCoursePage.js
+++ src/components/course/ManageCoursePage.js
@@ -11,7 +11,8 @@ class ManageCoursePage extends Component {
 
         this.state = {
             course: Object.assign({}, props.course),
-            errors: {}
+            errors: {},
+            saving: false
         };
         this.updateCourseState = this.updateCourseState.bind(this);
         this.saveCourse = this.saveCourse.bind(this);
@@ -31,12 +32,13 @@ class ManageCoursePage extends Component {
         return this.setState({ course });
     }
     saveCourse(event) {
-        event.stopPropagation();
         event.preventDefault();
+        this.setState({saving: true});
         this.props.actions.saveCourse(this.state.course)
             .then(() => this.redirect());
     }
     redirect(){
+        this.setState({saving: false});
         this.context.router.push('/courses');
     }
     render() {
@@ -48,6 +50,7 @@ class ManageCoursePage extends Component {
                     onSave={this.saveCourse}
                     course={this.state.course}
                     errors={this.state.errors}
+                    saving={this.state.saving}
                 />
             </div>
         )
