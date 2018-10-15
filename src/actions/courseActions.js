286a7ce4e9a2af444e045bc6315723f6b63bbdfa
maksym kolodiy <maksym.kolodiy.intern@3ds.com>
2018-10-15 17:36:53 +0200

    added ajax handle error action\reducer

--- src/actions/courseActions.js
+++ src/actions/courseActions.js
@@ -1,6 +1,7 @@
 import * as types from './actionTypes';
 import courseApi from '../api/mockCourseApi';
-import {beginAjaxCall} from './ajaxStatusActions'
+import {beginAjaxCall, ajaxCallError} from './ajaxStatusActions';
+
 
 export function loadCoursesSuccess(courses) {
     return {
@@ -38,6 +39,7 @@ export function saveCourse(course) {
             course.id ? dispatch(updateCourseSuccess(savedCourse)) :
                 dispatch(createCourseSuccess(savedCourse))
         }).catch(error => {
+            dispatch(ajaxCallError(error))
             throw (error);
         });
     }
--- src/reducers/ajaxStatusReducer.js
+++ src/reducers/ajaxStatusReducer.js
@@ -1,12 +1,13 @@
 import * as types from '../actions/actionTypes';
 import initialState from './initialState';
 
-const actionTypeEndsInSuccess = (type) => type.substring(type.length - 8) == '_SUCCESS'; 
+const actionTypeEndsInSuccess = (type) => type.substring(type.length - 8) == '_SUCCESS';
 
 export default (state = initialState.ajaxCallsInProgress, action) => {
     if (action.type == types.BEGIN_AJAX_CALL) {
         return state + 1;
-    } else if (actionTypeEndsInSuccess(action.type)) {
+    } else if (action.type == types.AJAX_CALL_ERROR ||
+        actionTypeEndsInSuccess(action.type)) {
         return state - 1;
     }
 
